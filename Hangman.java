c29c438343e44fdb1aa521b17dc3b1ec2cccc003
Aln999 <128022897+Aln999@users.noreply.github.com>
2024-09-23 19:33:39 +0530

    Add files via upload

---
+++ Hangman.java
@@ -0,0 +1,118 @@
+import java.util.Scanner;
+
+public class Hangman {
+
+    public static String[] words = {"ant", "baboon", "badger", "bat", "bear", "beaver", "camel",
+    "cat", "clam", "cobra", "cougar", "coyote", "crow", "deer",
+    "dog", "donkey", "duck", "eagle", "ferret", "fox", "frog", "goat",
+    "goose", "hawk", "lion", "lizard", "llama", "mole", "monkey", "moose",
+    "mouse", "mule", "newt", "otter", "owl", "panda", "parrot", "pigeon", 
+    "python", "rabbit", "ram", "rat", "raven","rhino", "salmon", "seal",
+    "shark", "sheep", "skunk", "sloth", "snake", "spider", "stork", "swan",
+    "tiger", "toad", "trout", "turkey", "turtle", "weasel", "whale", "wolf",
+    "wombat", "zebra"};
+
+    public static String[] gallows = 
+    {"+---+\n" +
+    "|   |\n" +
+    "    |\n" +
+    "    |\n" +
+    "    |\n" +
+    "    |\n" +
+    "=========\n",
+
+    "+---+\n" +
+    "|   |\n" +
+    "O   |\n" +
+    "    |\n" +
+    "    |\n" +
+    "    |\n" +
+    "=========\n",
+
+    "+---+\n" +
+    "|   |\n" +
+    "O   |\n" +
+    "|   |\n" +
+    "    |\n" +
+    "    |\n" +
+    "=========\n",
+
+    " +---+\n" +
+    " |   |\n" +
+    " O   |\n" +
+    "/|   |\n" +
+    "     |\n" +
+    "     |\n" +
+    " =========\n",
+
+    " +---+\n" +
+    " |   |\n" +
+    " O   |\n" +
+    "/|\\  |\n" + //if you were wondering, the only way to print '\' is with a trailing escape character, which also happens to be '\'
+    "     |\n" +
+    "     |\n" +
+    " =========\n",
+
+    " +---+\n" +
+    " |   |\n" +
+    " O   |\n" +
+    "/|\\  |\n" +
+    "/    |\n" +
+    "     |\n" +
+    " =========\n",
+
+    " +---+\n" +
+    " |   |\n" +
+    " O   |\n" +
+    "/|\\  |\n" + 
+    "/ \\  |\n" +
+    "     |\n" +
+    " =========\n"};
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String wordChoosed = words[randomGenerator()];
+        char[] word = wordChoosed.toCharArray();
+        for (int i = 0; i < wordChoosed.length(); i++) {
+            word[i] = '_';
+        }
+
+        printPlaceholder(word);
+
+        System.out.print("\nGuess : ");
+        char guess;
+        for (int i = 0; i < word.length; i++) {
+            guess = scan.next().charAt(i);
+
+        }
+
+
+
+    }
+    public static void printPlaceholder(char[] word){
+        System.out.print("\nWord : ");
+        for (int i = 0; i < word.length; i++) {
+            System.out.print(word[i] + " ");
+        }
+    }
+    public static int randomGenerator(){
+        return (int)(Math.random()*64)+1;
+    }
+    public static void printMissedGusses(char[] word,char guess){
+        
+    }
+    public static void updatePlaceholder(char[] word){
+
+    }
+    public static boolean checkGuess(char[] guess){
+        return true;
+    }
+
+}
+
+
+
+
+
